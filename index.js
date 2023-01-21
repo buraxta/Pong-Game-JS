27740b29fc670fcc8110d972afa213ffbaa2f610
Burak Güllüler <burakgulluler@gmail.com>
2023-01-21 16:04:06 +0300

    finished

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const paddleBorder = "black";
 const ballColor = "yellow";
 const ballBorderColor = "black";
 const ballRadius = 12.5;
-const paddleSpeed = 50;
+const paddleSpeed = 100;
 let intervalID;
 let ballSpeed;
 let ballX = gameWidth / 2;
@@ -59,17 +59,17 @@ function drawPaddles(){
     ctx.strokeStyle = paddleBorder;
 
     ctx.fillStyle = paddle1Color;
-    ctx.fillRect = (paddle1.x, paddle1.y, paddle1.height, paddle1.width);
-    ctx.strokeRect = (paddle1.x, paddle1.y, paddle1.height, paddle1.width);
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
     
     ctx.fillStyle = paddle2Color;
-    ctx.fillRect = (paddle2.x, paddle2.y, paddle2.height, paddle2.width);
-    ctx.strokeRect = (paddle2.x, paddle2.y, paddle2.height, paddle2.width);
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 
 };
 
 function createBall(){
-    ballSpeed = 1;
+    ballSpeed = 3;
     if (Math.round(Math.random()) == 1) {
         ballXDirection = 1;
     }
@@ -90,7 +90,7 @@ function moveBall(){
     ballX += (ballSpeed * ballXDirection);
     ballY += (ballSpeed * ballYDirection);
 }
-function drawBall(){
+function drawBall(ballX, ballY){
     ctx.fillStyle = ballColor;
     ctx.strokeStyle = ballBorderColor;
     ctx.lineWidth = 2;
@@ -112,7 +112,6 @@ function checkCollision(){
         createBall();
         return;
     }
-    }
     if (ballX >= gameWidth) {
         player1Score += 1;
         updateScore();
@@ -123,6 +122,70 @@ function checkCollision(){
         if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
             ballX = (paddle1.x + paddle1.width) + ballRadius; // if ball gets stuck
             ballXDirection *= -1;
-            ballSpeed += 1;
         }
     }
+    if (ballX >= (paddle2.x - ballRadius)) {
+        if (ballY > paddle2.y && ballY < paddle2.y + paddle2.height) {
+            ballX = paddle2.x - ballRadius; // if ball gets stuck
+            ballXDirection *= -1;
+        }
+    }
+};
+function changeDirection(event){
+    const keyPressed = event.keyCode;
+    const paddle1Up = 87;
+    const paddle1Down = 83;
+    const paddle2Up = 38;
+    const paddle2Down = 40;
+
+    switch (keyPressed) {
+        case(paddle1Up):
+            if (paddle1.y > 0) {
+                paddle1.y -= paddleSpeed;
+            }
+            break;
+        case(paddle1Down):
+            if (paddle1.y < gameHeight - paddle1.height) {
+                paddle1.y += paddleSpeed;
+            }
+            break;
+        case(paddle2Up):
+            if(paddle2.y > 0){
+                paddle2.y -= paddleSpeed;
+            }
+            break;
+        case(paddle2Down):
+            if (paddle2.y < gameHeight - paddle2.height) {
+                paddle2.y += paddleSpeed;
+            }
+            break;
+    }
+};
+function updateScore(){
+    scoreText.textContent = `${player1Score} : ${player2Score}`;
+}
+function resetGame(){
+    player1Score = 0;
+    player2Score = 0;
+    paddle1 = {
+        width: 25,
+        height: 100,
+        x: 0,
+        y: 0
+    }
+    paddle2 = {
+        width: 25,
+        height: 100,
+        x: gameWidth - 25,
+        y: gameHeight - 100
+    };
+    ballSpeed = 1;
+    ballX = 0;
+    ballY = 0;
+    ballXDirection = 0;
+    ballYDirection = 0;
+    updateScore();
+    clearInterval(intervalID);
+    gameStart();
+};
+    
